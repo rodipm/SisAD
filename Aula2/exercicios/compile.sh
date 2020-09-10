88034e643a87eac33f0cf07bc1839ccbbcb0b5d2
Rodrigo Perrucci Macharelli <rmacharelli@masterdom.inf.br>
2020-09-10 04:24:15 -0300

    ajuste -lpthread

--- Aula2/exercicios/compile.sh
+++ Aula2/exercicios/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -o mult-matrizes-seq ./mult-matrizes-seq.c -O3; gcc -o mult-matrizes-proc mult-matrizes-proc.c -O3; gcc -o mult-matrizes-pthreads mult-matrizes-pthreads.c -O3 -pthread
+gcc -o mult-matrizes-seq ./mult-matrizes-seq.c -O3 & gcc -o mult-matrizes-proc mult-matrizes-proc.c -O3 & gcc -o mult-matrizes-pthreads mult-matrizes-pthreads.c -O3 -lpthread
